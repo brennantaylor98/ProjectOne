e35e433438e631e22c47c5ca2fa8955828d98138
christiangella <112913805+christiangella@users.noreply.github.com>
2023-01-07 17:37:03 -0800

    comparing structures

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -74,7 +74,7 @@ try {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/dashboard');
+    res.redirect('/login');
     return;
   }
 
