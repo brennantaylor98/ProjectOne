b6184c4462aea4d769ddf0d9a121246ed3aebe21
christiangella <112913805+christiangella@users.noreply.github.com>
2023-01-10 11:28:59 -0800

    installing

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    'wallet_db',
+    'jqqeslnaxph7c33k',
+    'xnwq062bhxf4n5kx',
     {
       host: 'localhost',
       dialect: 'mysql',
