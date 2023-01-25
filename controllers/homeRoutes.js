295041508df98c4d7b6c67862142a15575c4b6dd
Brennan <paxtbiz@gmail.com>
2023-01-24 17:58:30 -0800

    updated render

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -84,13 +84,13 @@ router.get('/login', (req, res) => {
   return res.render('login');
 });
 
-router.get('/register', (req, res) => {
+router.get('/signup', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     return res.redirect('/dashboard');
   }
 
-  return res.render('register');
+  return res.render('signup');
 });
 
 module.exports = router;
